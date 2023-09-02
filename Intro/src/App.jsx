abcbf15a1e4a418ec61ef762412ecb12fea3417b
Sam Ly <samly@b98-aruba-authenticated-10-110-143-240.wlan.cpp.edu>
2023-09-01 17:03:14 -0700

    Project 1 setup

--- Intro/src/App.jsx
+++ Intro/src/App.jsx
@@ -1,13 +1,6 @@
-import reactLogo from "./assets/images/react-logo.png"
-
-import Header from "./Header"
-import MainContent from "./MainContent";
-
 function App() {
   return (
     <div className="app">
-      <Header />
-      <MainContent />
     </div>
   )
 }
