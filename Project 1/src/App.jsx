b7e086851c77c28057374a931737c292501e8dcd
Sam Ly <sam@samly.me>
2024-01-26 22:54:23 -0800

    complete intro and setup

---
+++ Project 1/src/App.jsx
@@ -0,0 +1,16 @@
+import reactLogo from "./assets/react-icon.png"
+
+const App = () => {
+    return (
+        <div>
+            <img src={reactLogo} alt="react-icon" width="80px" />
+            <h1>Fun facts balls React</h1>
+            <ul>
+                <li>Was first realesasdof asjdf</li>
+                <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, architecto saepe obcaecati ipsa, rem perferendis non ex impedit laboriosam consectetur sequi incidunt laudantium eos, nemo delectus? Distinctio minima repudiandae voluptas!</li>
+            </ul>
+        </div>
+    )
+}
+
+export default App;
