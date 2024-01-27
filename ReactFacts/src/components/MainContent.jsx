159df69b6d60eb43fab2c99b25bb8c8ebbb56dad
Sam Ly <sam@samly.me>
2024-01-27 01:05:45 -0800

    complete ReactFacts

---
+++ ReactFacts/src/components/MainContent.jsx
@@ -0,0 +1,18 @@
+const MainContent = () => {
+    return (
+        <div className="main-content">
+            <h1 className="main-content__title">
+                Fun facts about React
+            </h1>
+            <ul className="main-content__list">
+                <li className="main-content__list__item">Was first released in 2013</li>
+                <li className="main-content__list__item">Was originally created by Jordan Walke</li>
+                <li className="main-content__list__item">Has well over 100k stars on Github</li>
+                <li className="main-content__list__item">Is maintained by Facebook </li>
+                <li className="main-content__list__item">Powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
+        </div>
+    )
+}
+
+export default MainContent;
